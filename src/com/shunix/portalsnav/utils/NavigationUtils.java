112459219a5138012a98efd00bc69685085731c9
shunix <admin@shunix.com>
2013-07-29 23:47:31 +0800

    Design a class to parse the response JSON from Direction API

---
+++ src/com/shunix/portalsnav/utils/NavigationUtils.java
@@ -0,0 +1,22 @@
+package com.shunix.portalsnav.utils;
+
+import java.util.List;
+
+public final class NavigationUtils {
+	private static final String URL_STUB = "http://maps.googleapis.com/maps/api/directions/json?";
+	public static List<SingleStep> getDrivingSteps() {
+		//TODO
+	}
+	
+	public static List<SingleStep> getWalkingSteps() {
+		//TODO
+	}
+	
+	public static List<SingleStep> getBicyclingSteps() {
+		//TODO
+	}
+	
+	public static List<SingleStep> getTransitSteps() {
+		//TODO
+	}
+}
---
+++ src/com/shunix/portalsnav/utils/SingleStep.java
@@ -0,0 +1,14 @@
+package com.shunix.portalsnav.utils;
+
+public class SingleStep {
+	public String distance;
+	public String duration;
+	public Location startLocation;
+	public Location endLocation;
+	public String direction;
+}
+
+class Location {
+	public String lat;
+	public String lng;
+}
